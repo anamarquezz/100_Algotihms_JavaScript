baeb73a31535ed01fe5d7bf2467cd27c73e0daa7
AnaMarquez <anamarquez.edu@hotmail.com>
2021-05-24 22:02:22 -0700

    AddTwoDigits

---
+++ Algorithms/AddTwoDigits/AddTwoDigits.ts
@@ -0,0 +1,11 @@
+function addTwoDigits(n: any): number {
+    const nums = n.toString().split('');
+
+    return parseInt(nums[0]) + parseInt(nums[1]);
+/*
+    return nums.reduce((a: string, b: string) =>{
+        return parseInt(a) + parseInt(b);
+    });*/
+}
+
+console.log(addTwoDigits(29));

5287a9a9b881e5214375a2d29f9179e7aa8ffc8d
anamarquez <anamarquez.edu@hotmail.com>
2022-02-15 17:00:51 -0800

    areEquallyStrong

---
+++ Algorithms/areEquallyStrong/areEquallyStrong.ts
@@ -0,0 +1,8 @@
+function areEquallyString(yourLeft: number, yourRight number, friendsLeft: number, friendsRight: numner): boolean {
+ 
+
+}
+
+/*console.log(areEquallyString(10,15,15,10))
+console.log(areEquallyString(10,15,15,10))
+console.log(areEquallyString(10,15,15,10))*/

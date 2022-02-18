500bf38e5b0444ad9f08f4a1ec3eb98e5fcdaae9
anamarquez <anamarquez.edu@hotmail.com>
2022-02-17 16:58:12 -0800

    areEquallystrong, Are Similar

--- Algorithms/areEquallyStrong/areEquallyStrong.ts
+++ Algorithms/areEquallyStrong/areEquallyStrong.ts
@@ -1,8 +1,13 @@
-function areEquallyString(yourLeft: number, yourRight number, friendsLeft: number, friendsRight: numner): boolean {
- 
+function areEquallyStrong(yourLeft: number, yourRight:number, friendsLeft: number, friendsRight: numner): boolean {
+    const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+    const yourStrongest = yourLeft >= yourRight ? yourLeft: yourRight;
+    const friendsWeakest = friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+    const friendsSrtongest = friendsRight >= friendsRight ? friendsLeft: friendsRight;
+
+    return yourStrongest === friendsSrtongest && yourWeakest === friendsWeakest;
 
 }
 
-/*console.log(areEquallyString(10,15,15,10))
-console.log(areEquallyString(10,15,15,10))
-console.log(areEquallyString(10,15,15,10))*/
+console.log(areEquallyStrong(10,15,15,10))
+console.log(areEquallyStrong(10,15,15,10))
+console.log(areEquallyStrong(10,15,15,9))

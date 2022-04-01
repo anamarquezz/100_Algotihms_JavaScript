8b800ed3b991d9b1ca3b80676b2d3855a76140f6
anamarquez <anamarquez.edu@hotmail.com>
2022-04-01 16:17:52 -0700

    latest

--- example.ts
+++
@@ -1,6 +0,0 @@
-function add(num1: number, num2: number): number {
-    //const example: any[] = ["", 0, true];
-    //const example: any[] = [0, 1, 2];
-    const example: any[] = [[1, 2, 3]]; //more readable.
-    return num1 + num2;
-}
